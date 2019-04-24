e49ca8b4aed5d84dd1f9ada17bac0787f77d2c27
devils-K <polo.trial25@gmail.com>
2019-04-24 10:08:53 +0200

    fix

--- src/AlarmClock.Spa/src/components/Home/home_connected/clock_preset/index.vue
+++ src/AlarmClock.Spa/src/components/Home/home_connected/clock_preset/index.vue
@@ -5,7 +5,7 @@
       <div class="time" v-for="(time, index) in getTime(clock.time)" :key="time + index">{{time}}</div>
     </div>
     <div class="days-container">
-      <div class="day" v-for="(day, index) in days" :key="day.key" :class="{ active: isDayActive(clock, index)}">
+      <div class="day" v-for="(day, index) in days()" :key="day.key" :class="{ active: isDayActive(clock, index)}">
         {{formatDay(day)[0]}}
       </div>
     </div>
@@ -23,11 +23,12 @@ export default {
     }
   },
   data() {
-    return {
-      days
-    };
+    return {};
   },
   methods: {
+    days() {
+      return days
+    },
     getTime(time) {
       return time.split("");
     },
