9eb30134838c80b4f866491832abd0f8e3471c20
Thomas <thomas.cd@free.fr>
2019-04-26 14:45:32 +0200

    Insert statements

---
+++ src/AlarmClock.DB/Scripts/0023.Install-Insert-TestPreset.sql
@@ -0,0 +1,22 @@
+-- Insert rows into table 'spi.tAlarmPreset'
+INSERT INTO spi.tAlarmPreset
+    (
+    WakingTime,ActivationFlag,Challenge,Song,ClockId
+    )
+VALUES
+    (
+        '00010101', 0, 0, 'TestSong', 1
+    
+),
+    (
+        '00010101', 0, 0, 'TestSong 2', 1
+    
+),
+    (
+        '00010101', 0, 0, 'TestSong 3', 2
+    
+),
+    (
+        '00010101', 0, 0, 'TestSong 4', 2
+    
+)
