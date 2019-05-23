b84c6f02228e22fc1878f216c0ada7dc940161ef
Thomas <thomas.cd@free.fr>
2019-05-23 11:26:20 +0200

    1st gateway implementation

---
+++ AlarmClock.Device.SPA/src/AlarmClock.Device.DAL/Data/PresetData.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AlarmClock.Device.DAL.Data
+{
+    public class PresetData
+    {
+        public int AlarmPresetId { get; set; }
+        public TimeSpan WakingTime { get; set; }
+        public string Name { get; set; }
+        public string Song { get; set; }
+        public byte ActivationFlag { get; set; }
+        public int Challenge { get; set; }
+        public int ClockId { get; set; }
+    }
+}
