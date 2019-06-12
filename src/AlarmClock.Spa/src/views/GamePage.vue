0dfb491cd72d91b67f9a0d5d18d5a35e3fbdddb2
Anthony LASSERRE <lasserre@intechinfo.fr>
2019-06-12 09:07:03 +0200

    Add pixi dev tools + first snake ( not working ) + prepare to merge

---
+++ src/AlarmClock.Spa/src/views/GamePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <Snake></Snake>
+  </div>
+</template>
+
+<script>
+import Snake from "@/games/Snake/Snake.vue";
+
+export default {
+
+  components: {    
+      Snake: Snake
+  },
+
+  data() {
+
+    return {
+    }
+  },
+
+  computed: {
+  }
+
+};
+</script>
