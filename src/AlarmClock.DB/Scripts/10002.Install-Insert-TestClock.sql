a5ff56035c588c78a58456533ce95d764e1fca3d
Thomas <thomas.cd@free.fr>
2019-07-01 10:07:01 +0200

    fix insert

--- src/AlarmClock.DB/Scripts/10002.Install-Insert-TestClock.sql
+++ src/AlarmClock.DB/Scripts/10002.Install-Insert-TestClock.sql
@@ -4,11 +4,13 @@
  * File Created: Thursday,4th May 2019 09:07:47 am                             *
  * Author: Le Phoque Pirate                                                    *
  * --------------------                                                        *
- * Last Modified: Thursday, 13th June 2019 9:18:15 am                          *
+ * Last Modified: Monday, 1st July 2019 10:06:32 am                            *
  * Modified By: Le Phoque Pirate (tcousin@intechinfo.fr)                       *
  */
 
 DECLARE @ClockId INT
+DECLARE @guid UNIQUEIDENTIFIER = newid()
+DECLARE @guid2 UNIQUEIDENTIFIER = newid()
 
-EXECUTE spi.sCreateClock "Test-Clock-1",1, @ClockId output;
-EXECUTE spi.sCreateClock "Test-Clock-2", 1, @ClockId output;
+EXECUTE spi.sCreateClock "Test-Clock-1",@guid,1, @ClockId output;
+EXECUTE spi.sCreateClock "Test-Clock-2",@guid2, 1, @ClockId output;
